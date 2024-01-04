74769a8bec96c2711d10007a4dea3d34f24b71e3
ganga <gangadhar.vasa99@gmail.com>
2024-01-05 03:54:33 +0530

    completed

--- starter/app/src/main/java/com/udacity/shoestore/ShoeListingFragment.kt
+++ starter/app/src/main/java/com/udacity/shoestore/ShoeListingFragment.kt
@@ -2,7 +2,6 @@ package com.udacity.shoestore
 
 import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.Menu
 import android.view.MenuInflater
@@ -11,15 +10,13 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.LinearLayout
 import android.widget.TextView
-import androidx.activity.viewModels
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.NavController
 import androidx.navigation.NavOptions
 import androidx.navigation.findNavController
-import androidx.navigation.ui.NavigationUI
 import com.udacity.shoestore.databinding.FragmentShoeListingBinding
-import kotlin.time.Duration.Companion.nanoseconds
 
 
 /**
